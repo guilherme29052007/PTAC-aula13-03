80d01fcdcb85637a057ef5e6fed66ae1304ef888
joao <161075832+birulebe@users.noreply.github.com>
2024-03-05 08:00:20 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+
+  const nome = "João";
+  const sobreNome = "Santos";
+
+  return(
+    <div>
+        <h1>Hello World</h1>
+        <p>Desenvolvido por: {nome}</p>
+    </div>  
+  );
+
+
+}
